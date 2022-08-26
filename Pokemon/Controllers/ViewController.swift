66fbb6ac8f326b2d13d9c4313913fc54019f9f56
ItsMyDay797 <m.rus1298@gmail.com>
2022-08-26 14:35:25 +0300

    Added TableView

--- Pokemon/Controllers/ViewController.swift
+++ Pokemon/Controllers/ViewController.swift
@@ -7,37 +7,13 @@
 
 import UIKit
 
-let urlString = "https://pokeapi.co/api/v2/pokemon/"
-
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     
-      
-       print("works") //testing
         
     }
-   
-    func fetchPokemonData(completionHandler: @escaping ([PokemonData]) -> Void) {
-        
-        let url = URL(string: urlString)!
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            
-            guard let data = data else {return}
 
-            do{
-                let urlData = try JSONDecoder().decode([PokemonData].self, from: data)
-                
-                completionHandler(urlData)
-            }
-            catch {
-                let error = error
-                print(error.localizedDescription)
-            }
-            
-        }.resume()
-    }
 }
 
